651aaa26c4ea964cfbd52ed8e73534f6834d4217
gregolly <gregollyff@hotmail.com>
2023-10-13 06:37:00 -0400

    feat: update app and server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import fastify from 'fastify'
+import { fastify } from 'fastify'
 import cookie from '@fastify/cookie'
 
 import { mealsRoute } from './routes/meals-routes'
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app
-  .listen({
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log('http server running!')
-  })
+app.listen({ port: env.PORT }, () => {
+  console.log(`🚀 Server is running on port ${env.PORT} 🚀`)
+})
