ba8424733a6faf1853d2d7b6b4c1ab0dd946c1d7
gregolly <gregollyff@hotmail.com>
2023-10-13 06:37:26 -0400

    feat: meals and users routes

--- src/routes/meals-routes.ts
+++ src/routes/meals-routes.ts
@@ -1,3 +1,54 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { checkSessionIdSession } from '../middlewares/check-session-id-exists'
+import { z } from 'zod'
+import { knexDB } from '../database'
+import { randomUUID } from 'node:crypto'
 
-export async function mealsRoute(app: FastifyInstance) {}
+export async function mealsRoute(app: FastifyInstance) {
+  app.post(
+    '/',
+    { preHandler: [checkSessionIdSession] },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const createMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        date: z.coerce.date(),
+        time: z.string(),
+        onDiet: z.boolean(),
+      })
+
+      const validationResult = createMealBodySchema.safeParse(request.body)
+
+      if (!validationResult.success) {
+        return reply.status(400).send({
+          message: validationResult.error.errors,
+        })
+      }
+
+      const { name, description, date, time, onDiet } = validationResult.data
+
+      const { sessionId } = request.cookies
+
+      const { id } = await knexDB('users')
+        .select('id')
+        .where({ session_id: sessionId })
+        .first()
+
+      const meal = await knexDB('meals')
+        .insert({
+          id: randomUUID(),
+          name,
+          description,
+          date: date.toISOString(),
+          time,
+          on_diet: onDiet,
+          user_id: id,
+        })
+        .returning('*')
+
+        console.log(meal)
+
+      reply.status(201).send({ meal })
+    },
+  )
+}
--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -3,44 +3,86 @@ import { randomUUID } from 'node:crypto'
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
 
 import { knexDB } from '../database'
-
+import { checkSessionIdSession } from '../middlewares/check-session-id-exists'
 
 export async function usersRoutes(app: FastifyInstance) {
-    app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
-        const createUserBodySchema = z.object({
-            firstName: z.string().nonempty('First name cannobt be empty'),
-            lastName: z.string().nonempty('Last name cannot be empty'),
-            photoUrl: z.string().url().nonempty('Photo URL cannot be empty')
-        })
-
-        const validationResult = createUserBodySchema.safeParse(request.body)
-
-        if (!validationResult.success) {
-            return reply.status(400).send({
-                message: validationResult.error.errors
-            })
-        }
-
-        const { firstName, lastName, photoUrl } = validationResult.data
-
-        let sessionId = request.cookies.sessionId
-
-        if(!sessionId) {
-            sessionId = randomUUID()
-            reply.cookie('sessionId', sessionId, {
-                path: '/',
-                maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-            })
-        }
-
-        const user = await knexDB('users').insert({
-            id: randomUUID(),
-            first_name: firstName,
-            last_name: lastName,
-            photo_url: photoUrl,
-            session_id: sessionId,
-        }, '*')
-
-        reply.send({ user })
+  app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    const createUserBodySchema = z.object({
+      firstName: z.string().nonempty('First name cannobt be empty'),
+      lastName: z.string().nonempty('Last name cannot be empty'),
+      photoUrl: z.string().url().nonempty('Photo URL cannot be empty'),
     })
+
+    const validationResult = createUserBodySchema.safeParse(request.body)
+
+    if (!validationResult.success) {
+      return reply.status(400).send({
+        message: validationResult.error.errors.map((error) => error.message),
+      })
+    }
+
+    const { firstName, lastName, photoUrl } = validationResult.data
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    const user = await knexDB('users').insert(
+      {
+        id: randomUUID(),
+        first_name: firstName,
+        last_name: lastName,
+        photo_url: photoUrl,
+        session_id: sessionId,
+      },
+      '*',
+    )
+
+    reply.send({ user })
+  })
+
+  app.get(
+    '/metrics',
+    { preHandler: [checkSessionIdSession] },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const { sessionId } = request.cookies
+
+      const { id } = await knexDB('users')
+        .select('id')
+        .where({ session_id: sessionId })
+        .first()
+
+      const meals = await knexDB('meals')
+        .select('on_diet')
+        .where({ user_id: id })
+        .orderBy('upadted_at')
+
+      let bestStreak = 0
+      let currentSequence = 0
+
+      for (const meal of meals) {
+        if (meal.on_diet === 1) {
+          currentSequence++
+          bestStreak = Math.max(bestStreak, currentSequence)
+        } else {
+          currentSequence = 0
+        }
+      }
+
+      const onDiet = meals.filter((meal) => meal.on_diet).length
+
+      reply.send({
+        total: meals.length,
+        onDiet,
+        offDiet: meals.length - onDiet,
+        bestStreak,
+      })
+    },
+  )
 }
